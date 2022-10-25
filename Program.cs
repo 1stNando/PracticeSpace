ea911a9af15b87dde5782e1178947a86de473f7e
Fernando Aguilar <sparkmanperspective@gmail.com>
2022-10-24 22:36:17 -0400

    Simple Calculator

--- Program.cs
+++ Program.cs
@@ -1,12 +1,93 @@
 ﻿using System;
-
-namespace PracticeSpace
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
+namespace PlainPractice
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            bool isMale = false;
+            bool isTall = true;
+
+            if (isMale && isTall)
+            {
+                Console.WriteLine("Your a tall male");
+            }
+            else if (isMale && !isTall)
+            {
+                Console.WriteLine("You are short");
+            }
+            else if (!isMale && isTall)
+            {
+                Console.WriteLine("You are not a male but your are tall");
+            }
+            else
+            {
+                Console.WriteLine("You are not male and not tall");
+            }
+            // Here we are finally calling the GetMax method we first had to put together bellow the Main.
+            // Dont forget to give the method the two values needed to start the comparison. Returns bigger#.
+            Console.WriteLine(GetMax(20, 10, 40));
+            Console.ReadLine();
+
+            //New assignment: build a simple calculator.
+            // - First we need a prompt to ask user for a number
+            Console.Write("Enter a number: ");
+            Console.ReadLine();
+
+        }
+
+        //Comparison statements within if 
+        //We want this method to figure out which of the two numbers is bigger. And then return it to the caller
+        // static int GetMax(int num1, int num2)
+        // {
+        //     int result;
+        //     //lets use an if statement to apply this logic. A true or false condition goes inside the ().
+        //     if (num1 > num2)//this compares the two. And resolves down to a true/false. Only two scenarios.
+        //     {
+        //         result = num1;
+        //     }
+        //     else
+        //     {
+        //         result = num2;
+        //     }
+
+        //     return result;
+        // }//With this method done. We can now CALL IT in the Main!
+
+        //this next version of GetMax will include a 3rd #.
+        static int GetMax(int num1, int num2, int num3)
+        {
+            int result;
+            //Now we need a condition to check the 3.
+            if (num1 >= num2 && num1 >= num3)
+            {
+                result = num1;
+            }
+            else if (num2 >= num1 && num2 >= num3)
+            {
+                result = num2;
+            }
+            else
+            {
+                result = num3;
+            }
+
+
+
+            return result;
+        }//With this method done. We can now CALL IT in the Main!
+
+
+
+
+
+
+
+
+
     }
-  }
 }
+
--- README.md
+++ README.md
@@ -1,1 +1,62 @@
 # PracticeSpace
+
+            /////////////////////////////////////////////////////////////////////////////////////////////
+            //This is a method
+            static void DisplayGreeting()
+            {
+                Console.WriteLine("-------------------");
+                Console.WriteLine("Welcome to Our Employee Database");
+                Console.WriteLine("-------------------");
+            }
+
+            //Our method signature for a return input.
+            //static method. The return (output) type. This says that we expect this method to return a single string to the code that called it. The inputs, known as arguments. In this case a single string in a variable known as 'prompt'.
+            static string PromptForString(string prompt)
+            {
+                //Add some emoji every time this prompt is used
+                Console.WriteLine("xxxxx");
+                //Use the argument, whatever the method caller sent us.
+                Console.Write(prompt);
+                //Get some user input
+                var userInput = Console.ReadLine();
+                //ReTURN that value as the output of this method. The calue in 'userInput' will go wherever the CALLER of the method has specified.
+                return userInput;
+            }
+
+            static int PromptForInteger(string prompt)
+            {
+                Console.Write(prompt);
+                var userInput = Console.ReadLine();
+                var inputAsInteger = int.Parse(userInput);
+                return inputAsInteger;
+            }
+
+            //Call the method for display greeting
+            DisplayGreeting();
+            //Console.Write("What is your name? ");
+            //var name = Console.ReadLine();
+            //lets redo the above for a more expressive, even better way. Reveals the intent without details.
+            var name = PromptForString("What is your name? ");
+
+            //Console.Write("What is your department number? ");
+            //var department = int.Parse(Console.ReadLine());
+            //Lets replace the above department with the new prompt for integer we just created.
+            var department = PromptForInteger("What is your department number? ");
+
+            //Console.Write("What is your yearly salary (in dollars)? ");
+            //var salary = int.Parse(Console.ReadLine());
+            var salary = PromptForInteger("What is your yearly salary (in dollars)? ");
+
+            var salaryPerMonth = salary / 12;
+            Console.WriteLine($"Hello, {name} you make {salaryPerMonth} a month. ");
+
+
+
+
+
+
+
+
+
+        }
+    }
